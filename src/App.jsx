849b79fb672e2cb9873c0c23ad6ba1d089dc67ac
Hadia Inam <hadia.inam@outlook.com>
2025-09-30 23:13:59 -0500

    Create Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Login from './pages/Login.jsx'
+import Profile from './pages/Profile.jsx'
+import MyAppointments from './pages/MyAppointments.jsx'
+import Appointment from './pages/Appointment.jsx'
+import Doctors from './pages/Doctors.jsx'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/my-appointments' element={<MyAppointments/>} />
+        <Route path='/appointment/:docId' element={<Appointment/>} />
+        <Route path='/doctors/:speciality' element={<Doctors/>} />
+        <Route path='/doctors' element={<Doctors/>} />
+      </Routes>
+      <Footer />
+    </div>
   )
 }
 
